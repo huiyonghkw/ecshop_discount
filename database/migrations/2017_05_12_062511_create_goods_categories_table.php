d5534634a6a1d66a17e40ce7659c86f30e9e2be7
chenghuiyong <chenghuiyong@weipei.cc>
2017-05-12 14:25:27 +0800

    php artisan make:model App/Models/GoodsCategory -m

---
+++ database/migrations/2017_05_12_062511_create_goods_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoodsCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods_categories');
+    }
+}
