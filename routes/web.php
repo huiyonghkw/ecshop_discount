682add9e1c425384eb235ec77c54034c5d99c19a
chenghuiyong <chenghuiyong@weipei.cc>
2017-05-12 17:59:32 +0800

    php artisan make:auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Auth::routes();
+Route::group(
+	[
+	    'namespace' => 'Web', 
+	],
+	function ($route) {
+	    $route->resource('goodses', 'GoodsController');
+	}
+);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
